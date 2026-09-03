aca902fac8a8de58381da59470315e50c109da36
Konrad <slaves908@gmail.com>
2026-09-03 13:11:37 +0200

    Poprawa Google Analytics

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,23 +1,29 @@
 import Script from "next/script";
 
-const GA_ID = "G-6P53DQG8PR";
+const GOOGLE_TAG_ID = "G-6P53DQG8PR";
 
 export default function GoogleAnalytics() {
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        id="google-tag-script"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`}
         strategy="afterInteractive"
       />
 
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script
+        id="google-tag"
+        strategy="afterInteractive"
+      >
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){window.dataLayer.push(arguments);}
           window.gtag = gtag;
 
           gtag('js', new Date());
-          gtag('config', '${GA_ID}');
+          gtag('config', '${GOOGLE_TAG_ID}', {
+            page_path: window.location.pathname,
+          });
         `}
       </Script>
     </>
